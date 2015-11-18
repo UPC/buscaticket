17845321c98ae99ca2778044ce99113fb0559743
Jaume Moral <jaume.moral@gmail.com>
2015-11-19 00:15:52 +0100

    Afegim json amb tickets oberts setmanals

--- buscador.py
+++ buscador.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask import render_template
-from flask import request
+from flask import request,jsonify
 
-from buscar import busca
+from buscar import Consultes
 app = Flask(__name__)
 
 @app.route("/")
 def buscador():
 	q = request.args.get('q', '')
-	l=busca(q)
+	c=Consultes()
+	l=c.busca(q)
 	return render_template('buscador.html', resultats=l,q=q)
+
+@app.route("/oberts-setmanals")
+def oberts_setmanals():
+	c=Consultes()
+	return jsonify(c.oberts_setmanals())
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- buscar.py
+++ buscar.py
@@ -1,11 +1,48 @@
 import sys
 import pymongo
 
-def busca(text):
-  client = pymongo.MongoClient()  
-  db=client.ticketsdb
-  return list(db.tickets.find({"$text":{"$search":text}})
-  	                    .sort([("estat",pymongo.ASCENDING),("dataTancament",pymongo.DESCENDING), ("dataCreacio",pymongo.DESCENDING)])
-  )
+class Consultes:
 
+  def __init__(self):
+    client = pymongo.MongoClient()  
+    self.db=client.ticketsdb    
 
+  def busca(self,text):
+    return list(self.db.tickets.find({"$text":{"$search":text}})
+     .sort([("estat",pymongo.ASCENDING),("dataTancament",pymongo.DESCENDING), ("dataCreacio",pymongo.DESCENDING)])
+     )
+
+  def obrim_setmanals(self):
+    return [{"_id":{"any":2014,"setmana":10},"tickets":2},
+            {"_id":{"any":2015,"setmana":3},"tickets":2}]
+
+  def tanquem_setmanals(self):
+    return [{"_id":{"any":2014,"setmana":11},"tickets":1},
+            {"_id":{"any":2015,"setmana":6},"tickets":2}]
+
+  def oberts_setmanals(self):
+    obrim=self.obrim_setmanals()
+    tanquem=self.tanquem_setmanals()
+    obrim_map={}
+    tanquem_map={}
+    for t in obrim: obrim_map[(t["_id"]["any"],t["_id"]["setmana"])]=t["tickets"]
+    for t in tanquem: tanquem_map[(t["_id"]["any"],t["_id"]["setmana"])]=t["tickets"]
+    any_inici=obrim[0]["_id"]["any"]
+    any_fi=tanquem[-1]["_id"]["any"]
+    n_oberts=0
+    tickets=[]
+    for a in range(any_inici,any_fi+1):
+      for s in range(0,52):
+        try:
+          n_obrim=obrim_map[(a,s)]
+        except:
+          n_obrim=0
+        try:
+          n_tanquem=tanquem_map[(a,s)]
+        except:
+          n_tanquem=0          
+        n_oberts+=n_obrim-n_tanquem
+        linia={"_id":{"any":a,"setmana":s},"obrim":n_obrim,"tanquem":n_tanquem,"oberts":n_oberts}
+        tickets.append(linia)
+        print linia
+    return {"oberts-setmanals":tickets}
